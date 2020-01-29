49e0f3deaece28423a719beab3556149e9f9f895
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-01-30 10:34:19 +1100

    still testing

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module BurningRails
     # config/initializers/cors.rb
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:4200'
+        origins '*'
         resource '*',
           headers: :any,
           methods: %i(get post put patch delete options head)
