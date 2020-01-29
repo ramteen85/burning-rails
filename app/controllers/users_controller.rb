a676e7bf921dc66f46e6a84d614b11e8e29a6a2f
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-01-30 01:52:19 +1100

    fixed up routes, return more json, fixed up user controller... still work to do

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,63 @@
 class UsersController < ApplicationController
+
+    before_action :allow_cors
+
     def index
-    
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def new 
+        @user = User.new
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            session[:user_id] = user.id
-            render json: {
-                message: "registration successful",
-                logged_in: true
-            }
-        else
-            render json: {
-                message: user.errors.full_messages,
-                logged_in: false
-            }
+        @user = User.new(user_params)
+
+        respond_to do |format|
+          if @user.save
+            render json: @user 
+          else
+            # might need to edit this
+            render json: @user.errors
+          end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @user.update(user_params)
+            render json: @user
+            else
+                render json: @user.errors
+            end
+        end
+    end
+
+    def destroy
+        @user.destroy
+        respond_to do |format|
+            @deleted = { message: "User Deleted" }
+            render json: @deleted
         end
     end
 
     private
         def user_params
             params.require(:user).permit(:name, :password, :password_confirmation)
+        end
+
+        def allow_cors
+            headers['Access-Control-Allow-Origin'] = '*'
+        end
+    
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def user_params
+            params.require(:user).permit(:user_id)
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  resources :flights
   resources :planes
+  resources :sessions
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/' => 'users#index'
-  post '/sessions' => 'sessions#create'
-  post '/users' => 'users#create'
-  get '/dashboard' => 'planes#index'
+  resources :planes
 end
